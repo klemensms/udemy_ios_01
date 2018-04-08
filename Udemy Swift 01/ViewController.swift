e1ef2cad39c578da20cef5f6cd7c7b5fcdbca7c4
Klemens Michael Stelk <klemens@stelk.de>
2018-04-08 12:17:48 +0530

    second commit

--- Udemy Swift 01/ViewController.swift
+++ Udemy Swift 01/ViewController.swift
@@ -3,7 +3,7 @@
 //  Udemy Swift 01
 //
 //  Created by Klemens Stelk on 08/04/2018.
-//  Copyright © 2018 Klemens Stelk. All rights reserved.
+//  Copyright © 2018 Klemens Stelk. All rights reserved...
 //
 
 import UIKit
