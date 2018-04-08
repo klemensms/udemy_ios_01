d8e081f1700e948449ccd05e78697fbb93a17805
Klemens Michael Stelk <klemens@stelk.de>
2018-04-08 12:11:02 +0530

    first commit.

--- Udemy Swift 01/ViewController.swift
+++ Udemy Swift 01/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLable: UILabel!
+    
 
+    @IBAction func buttenTapped(_ sender: Any) {
+        
+        buttonCount += 1
+        print(buttonCount)
+        if buttonCount > 3 {
+            buttonCount = 0
+            view.backgroundColor = UIColor.white
+            myLable.text = "Hello World!!!!!"
+        }
+        if buttonCount == 3 {
+        
+        view.backgroundColor = UIColor.red
+        myLable.text = "My name is Klemens"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
